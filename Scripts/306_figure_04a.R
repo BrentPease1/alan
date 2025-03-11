8bdf9ab127b288e8e13a81a8bbbd4702193918de
Gilbert <neil.gilbert@okstate.edu>
2025-03-11 14:19:36 -0500

    updating figure 4 to show evening results

--- Scripts/306_figure_04a.R
+++ Scripts/306_figure_04a.R
@@ -53,26 +53,48 @@ my_tree$tip.label <- family_map[my_tree$tip.label]
 
 setwd(here::here("Results"))
 load("tmb_onset_models_family.RData")
+load("tmb_evening_models_family.RData")
 
 m1_ranef <- glmmTMB::ranef(m1)
+e1_ranef <- glmmTMB::ranef(e1)
 
 fam_re <- m1_ranef$cond$family |> 
   tibble::as_tibble(rownames = "tip.label") |> 
   janitor::clean_names() |> 
-  dplyr::rename(tip.label = tip_label)
+  dplyr::rename(tip.label = tip_label,
+                int.m = intercept, 
+                slope.m = alan_sc)
+
+fam_re_e <- e1_ranef$cond$family |> 
+  tibble::as_tibble(rownames = "tip.label") |> 
+  janitor::clean_names() |> 
+  dplyr::rename(tip.label = tip_label,
+                int.e = intercept, 
+                slope.e = alan_sc)
 
 fam_bi <- fam_re |> 
-  dplyr::mutate(alan_cat = ifelse(alan_sc < 0, 1, 2), 
-                int_cat = ifelse(intercept < 0, 1, 2)) |> 
-  dplyr::mutate(bi_class = paste(int_cat, alan_cat, sep = "-"))
+  dplyr::left_join(
+    fam_re_e) |> 
+  dplyr::mutate( int.m.cat = ifelse(int.m < 0, 1, 2), 
+                 slope.m.cat = ifelse( slope.m < 0, 1, 2),
+                 
+                 int.e.cat = ifelse(!is.na(int.e) & int.e > 0, 1,
+                                    ifelse(!is.na(int.e) & int.e < 0, 2, NA)),
+                 slope.e.cat = ifelse(!is.na(slope.e) & slope.e > 0, 1, 
+                                      ifelse(!is.na(slope.e) & slope.e < 0, 2, NA))) |> 
+  dplyr::mutate(bi.m = paste(int.m.cat, slope.m.cat, sep = "-"),
+                bi.e = ifelse(is.na(int.e.cat), NA, paste(int.e.cat, slope.e.cat, sep = "-"))) |> 
+  dplyr::select(tip.label, bi.m, bi.e) 
 
 p <- ggtree::ggtree( my_tree, layout = "rectangular", linewidth = 0.2)
 
-p2 <- p %<+% fam_bi + 
-  geom_tippoint( aes(color = bi_class), size = 1.5) +
-  biscale::bi_scale_color(pal = "BlueOr", dim = 2, rotate_pal = TRUE) +
+p2 <-
+  p %<+% fam_bi + 
+  geom_tippoint( aes(color = bi.m), size = 1.5) +
+  geom_tippoint( aes(color = bi.e), size = 1.5, position = position_nudge(x = 3)) + 
+  biscale::bi_scale_color(pal = "BlueOr", na.value = "white", dim = 2, rotate_pal = TRUE) +
   theme(legend.position = "none") +
-  geom_tiplab(aes(color = bi_class), offset = 1.5, size = 2.5) +
+  geom_tiplab(offset = 4.1, size = 2.5) +
   theme(plot.margin = unit(c(2, 20, 2, 2), "mm")) +
   coord_cartesian(clip = "off")
     
@@ -81,16 +103,19 @@ leg <- bi_legend(
             xlab = "Baseline onset",
             ylab = "Light pollution effect",
             size = 9,
-            arrows = FALSE) 
+            arrows = FALSE) +
+  theme(plot.background = element_rect(fill = NA, color = NA), 
+        panel.background = element_rect(fill = NA, color = NA))
 
 cowplot::ggdraw() +
   draw_plot( p2 ) +
-  draw_plot( leg, 0.08, 0.65, 0.4, 0.4)
+  draw_plot( leg, 0.03, 0.55, 0.47, 0.47)
 
 setwd(here::here("Results/Figures"))
+
 ggsave(
   filename = "figure_04a.png", 
   width = 4.25, 
-  height = 7.2, 
+  height = 7.25, 
   units = "in", 
   dpi = 600)
